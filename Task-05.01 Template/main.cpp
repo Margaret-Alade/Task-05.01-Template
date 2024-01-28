31c090c1d788c010cf6a9873a4fece55e9e84f3b
Margaret Alade <alade.margaret@yandex.ru>
2024-01-28 16:00:16 +0300

    Initial commit

--- Task-05.01 Template/main.cpp
+++ Task-05.01 Template/main.cpp
@@ -6,9 +6,45 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+
+template <class T>
+T squaring (T value) {
+    return pow(value, 2);
+}
+
+template <>
+std::vector<int> squaring(std::vector<int> vec) {
+    
+    for (int i : vec) {
+        pow(i, 2);
+    }
+    return vec;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "[IN]: ";
+    int num = 4;
+    std::cout << num;
+    std::cout << "\n";
+    std::cout << "[OUT]: " << squaring<int>(num) << "\n";
+
+    std::cout << "[IN]: ";
+    
+    std::vector<int> vec{-1, 4, 8};
+    
+    for (int i : vec) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+    
+    std::cout << "[OUT]: ";
+    
+    for (int i : squaring<std::vector<int>>(vec)) {
+        std::cout << i << " ";
+    }
+    
     return 0;
 }
